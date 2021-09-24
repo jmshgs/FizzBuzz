dd3608c3e281291add72155a51deade178517c61
Revest <72482214+revestyt@users.noreply.github.com>
2021-09-24 21:47:17 +0000

    changed indents

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 for i in range(51):
-  if i % 3 == 0 and i % 5 == 0:
-    print("fizzbuzz")
-  elif i % 3 == 0:
-    print("fizz")
-  elif i % 5 == 0:
-    print("buzz")
-  print(i)
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    print(i)
