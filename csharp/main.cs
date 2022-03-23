53b9c46e14e1ca740bf1d0f8401390e85a8ce663
jam3shug <jameshug678@gmail.com>
2022-03-23 14:32:53 -0700

    Add files via upload

---
+++ csharp/main.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Main
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 50; i++)  
+        {  
+            if (i % 3 == 0 && i % 5 == 0)  
+            {  
+                Console.WriteLine(i + " fizzbuzz");  
+            }  
+            else if (i % 3 == 0)  
+            {  
+               Console.WriteLine(i + " fizz");  
+            }  
+            else if (i % 5 == 0)  
+            {  
+               Console.WriteLine(i + " buzz");  
+            }  
+            else  
+            {  
+                Console.WriteLine(i);  
+            }  
+        }
+    }  
+} 
